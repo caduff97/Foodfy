6e9269aa38c333c51b01b5051ebcefa1f366d5ff
caduff97 <kadufernandes.kf@gmail.com>
2021-01-13 23:29:32 -0300

    progress in challenge

--- public/scripts.js
+++ public/scripts.js
@@ -319,3 +319,18 @@ const ImagesUpload = {
         imagesDiv.remove()
     }
 }
+
+const ImageGallery = {
+    highlight: document.querySelector(".gallery .highlight > img"),
+    previews: document.querySelectorAll(".gallery-preview img"),
+    setImage(e) {
+        const { target } = e
+
+        ImageGallery.previews.forEach(preview => preview.classList.remove("active"))
+        target.classList.add("active")
+
+        ImageGallery.highlight.src = target.src
+        ImageGallery.highlight.alt = target.alt
+        Lightbox.image.src = target.src
+    }
+}
